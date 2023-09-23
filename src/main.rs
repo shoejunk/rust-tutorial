99ec38d5f1d32ec4e064ed0317fdcbd9ffd884df
Joseph Shunk <joseph.shunk@gmail.com>
2023-09-22 23:49:48 -0700

    Random number

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
@@ -12,5 +17,16 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
+
+    // if guess < secret_number
+    // {
+    //     println!("Too low!");
+    // }
+    // else if guess > secret_number {
+    //     println!("Too high!");
+    // }
+    // else {
+    //     println!("You guessed it!");
+    // }
     
 }
